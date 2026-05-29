671570298c8d0ffead2553b0bbd60d2b53ada1a4
thien <thienanhheo2@gmail.com>
2026-05-29 08:02:47 +0700

    playfair

--- LAB-02/ex01/api.py
+++ LAB-02/ex01/api.py
@@ -63,3 +63,64 @@ def decrypt():
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({"decrypted_text": decrypted_text})
+
+from cipher.playfair import PlayFairCipher
+
+# Thêm vào phần đầu file api.py
+
+# =========================
+# PLAYFAIR CIPHER ALGORITHM
+# =========================
+
+playfair_cipher = PlayFairCipher()
+
+
+@app.route('/api/playfair/creatematrix', methods=['POST'])
+def playfair_creatematrix():
+    data = request.json
+
+    key = data['key']
+
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+
+    return jsonify({
+        "playfair_matrix": playfair_matrix
+    })
+
+
+@app.route('/api/playfair/encrypt', methods=['POST'])
+def playfair_encrypt():
+    data = request.json
+
+    plain_text = data['plain_text']
+    key = data['key']
+
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+
+    encrypted_text = playfair_cipher.playfair_encrypt(
+        plain_text,
+        playfair_matrix
+    )
+
+    return jsonify({
+        'encrypted_text': encrypted_text
+    })
+
+
+@app.route('/api/playfair/decrypt', methods=['POST'])
+def playfair_decrypt():
+    data = request.json
+
+    cipher_text = data['cipher_text']
+    key = data['key']
+
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+
+    decrypted_text = playfair_cipher.playfair_decrypt(
+        cipher_text,
+        playfair_matrix
+    )
+
+    return jsonify({
+        'decrypted_text': decrypted_text
+    })
