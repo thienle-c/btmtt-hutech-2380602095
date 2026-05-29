30396ba3aa34f6bf9df0bc53fe81b9ed8d94e0a9
thien <thienanhheo2@gmail.com>
2026-05-29 10:08:09 +0700

    Postman test

--- LAB-02/ex01/cipher/vigenere/vigenere_cipher.py
+++ LAB-02/ex01/cipher/vigenere/vigenere_cipher.py
@@ -1,4 +1,4 @@
-class vigenereCipher:
+class VigenereCipher:
     def __init__(self):
         pass
 
@@ -32,17 +32,3 @@ class vigenereCipher:
                 decrypted_text += char
         return decrypted_text
     
-def vigenere_decrypt(self, encrypted_text, key):
-    decrypted_text = ""
-    key_index = 0
-    for char in encrypted_text:
-        if char.isalpha():
-            key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
-            if char.isupper():
-                decrypted_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
-            else:
-                decrypted_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
-            key_index += 1
-        else:
-            decrypted_text += char
-    return decrypted_text
